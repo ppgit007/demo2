4dccd77b3b228fa28783bc67ca887cc3a21d9d10
kumari <850034498@genpact.com>
2018-09-18 14:58:58 +0530

    new commit in demo2

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		System.out.println("This is just a string one");
+		System.out.println("This is just a string two   ");
+	}
+	
+}
