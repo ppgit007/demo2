693f915377ff71400e5abd64aa3c77320db37b32
kumari <850034498@genpact.com>
2018-09-18 15:17:34 +0530

    Commit in myfeature brance

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args){
 		System.out.println("This is just a string one");
-		System.out.println("This is just a string two   ");
+		System.out.println("This is just a string two");
+		System.out.println("This is just a string three");
 	}
 	
 }
